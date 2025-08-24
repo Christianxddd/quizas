fb627f947b3d43d20b1f07fe27a75fee9e820683
Christianxddd <jostin10045@gmail.com>
2025-08-23 20:42:37 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,78 @@
-local player = game.Players.LocalPlayer local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))() local Window = OrionLib:MakeWindow({Name = "Example Hub (Rename This!)", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"}) --[[ Name = - The name of the UI. HidePremium = - Whether or not the user details shows Premium status or not. SaveConfig = - Toggles the config saving in the UI. ConfigFolder = - The name of the folder where the configs are saved. IntroEnabled = false - Whether or not to show the intro animation. IntroText = - Text to show in the intro animation. IntroIcon = - URL to the image you want to use in the intro animation. Icon = - URL to the image you want displayed on the window. CloseCallback = - Function to execute when the window is closed. ]] local Tab = Window:MakeTab({ Name = "Tab 1", Icon = "rbxassetid://4483345998", PremiumOnly = false }) --[[ Name = - The name of the tab. Icon = - The icon of the tab. PremiumOnly = - Makes the tab accessible to Sirus Premium users only. ]] local Section = Tab:AddSection({ Name = "LocalPlayer" }) --[[ Name = - The name of the section. ]] OrionLib:MakeNotification({ Name = "Welcome!", Content = "Welcome to my hub!", Image = "rbxassetid://4483345998", Time = 5 }) --[[ Title = - The title of the notification. Content = - The content of the notification. Image = - The icon of the notification. Time = - The duration of the notfication. ]] Tab:AddButton({ Name = "High Speed", Callback = function() player.Character.Humanoid.WalkSpeed = 500 end }) --[[ Name = - The name of the button. Callback = - The function of the button. ]] Tab:AddButton({ Name = "High Jumppower", Callback = function() player.Character.Humanoid.JumpPower = 100 end }) --[[ Name = - The name of the button. Callback = - The function of the button. ]] Tab:AddButton({ Name = "Low Gravity", Callback = function() game.Workspace.Gravity = 10 end }) --[[ Name = - The name of the button. Callback = - The function of the button. ]] --[[ Name = - The name of the button. Callback = - The function of the button. ]]
+-- // Local Player
+local player = game.Players.LocalPlayer
+
+-- // Load Orion Library
+local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
+
+-- // Create Main Window
+local Window = OrionLib:MakeWindow({
+    Name = "Example Hub (Rename This!)",
+    HidePremium = false,
+    SaveConfig = true,
+    ConfigFolder = "OrionTest"
+    --[[ 
+        Name =           The name of the UI
+        HidePremium =    Whether or not the user details shows Premium status
+        SaveConfig =     Toggles config saving in the UI
+        ConfigFolder =   Folder name where configs are saved
+        IntroEnabled =   Show intro animation? (true/false)
+        IntroText =      Text in intro animation
+        IntroIcon =      Icon in intro animation
+        Icon =           Window icon
+        CloseCallback =  Function executed when window closes
+    ]]
+})
+
+-- // Create Tab
+local Tab = Window:MakeTab({
+    Name = "Tab 1",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+    --[[ 
+        Name =        The name of the tab
+        Icon =        Icon of the tab
+        PremiumOnly = Only accessible for Premium users
+    ]]
+})
+
+-- // Create Section
+local Section = Tab:AddSection({
+    Name = "LocalPlayer"
+    --[[ Name = The name of the section ]]
+})
+
+-- // Welcome Notification
+OrionLib:MakeNotification({
+    Name = "Welcome!",
+    Content = "Welcome to my hub!",
+    Image = "rbxassetid://4483345998",
+    Time = 5
+    --[[ 
+        Name =    Title of the notification
+        Content = Content text
+        Image =   Notification icon
+        Time =    Duration of the notification
+    ]]
+})
+
+-- // Buttons
+Tab:AddButton({
+    Name = "High Speed",
+    Callback = function()
+        player.Character.Humanoid.WalkSpeed = 500
+    end
+})
+
+Tab:AddButton({
+    Name = "High Jumppower",
+    Callback = function()
+        player.Character.Humanoid.JumpPower = 100
+    end
+})
+
+Tab:AddButton({
+    Name = "Low Gravity",
+    Callback = function()
+        game.Workspace.Gravity = 10
+    end
+})
